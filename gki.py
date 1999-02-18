899fdfe3e45de9d654cd08b372eccfde99486b7d
rlw <rlw@stsci.edu>
1999-02-18 19:22:14 +0000

    Made GkiBuffer more sophisticated so that it contains a pointer telling how much metacode has been translated and it includes much of the metacode stream parsing originally in GkiTranslate() function. Added access functions gwm.getActiveWindow() and gwm.getActiveWindowTop() and eliminated external access to gwm.g, the window manager object.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@40 05fa7843-0683-0410-9e38-dfee850dc5be

--- gki.py
+++ gki.py
@@ -8,12 +8,13 @@ from types import *
 import gwm
 
 BOI = -1  # beginning of instruction sentinel
-NOP = 0	 # no op value
+NOP = 0	  # no op value
 GKI_MAX = 32767
 GKI_MAX_FLOAT = Numeric.array(GKI_MAX,Numeric.Float32)
 GKI_MAX_OP_CODE = 27
 
 # need to treat this more generally
+# Could do a test with struct & byte-swapping option to determine byte order
 
 BYTESWAP = 0
 
@@ -44,28 +45,50 @@ GKI_ESCAPE = 25
 GKI_SETWCS = 26
 GKI_GETWCS = 27
 
+GKI_ILLEGAL_LIST = (21,22,23,24)
+
 class GkiBuffer:
 
 	"""implement a buffer for gki which allocates memory in blocks so that 
 	a new memory allocation is not needed everytime metacode is appended"""
 
 	INCREMENT = 50000
+
+	def __init__(self, metacode=None):
 	
-	def __init__(self):
-	
-		self.reset()	
-	
+		if metacode:
+			self.buffer = metacode
+			self.bufferSize = len(metacode)
+			self.bufferEnd = len(metacode)
+			# nextTranslate is pointer to next element in buffer to be
+			# translated.  It is needed because we may get truncated
+			# messages, leaving some metacode to be prepended to next
+			# message.
+			self.nextTranslate = 0
+		else:
+			self.buffer = None
+			self.bufferSize = 0
+			self.bufferEnd = 0 
+			self.nextTranslate = 0
+
 	def reset(self):
 
-		self.buffer = None
-		self.bufferSize = 0
-		self.bufferEnd = 0 
-		
+		# discard everything up to nextTranslate pointer
+
+		newEnd = self.bufferEnd - self.nextTranslate
+		if newEnd > 0:
+			self.buffer[0:newEnd] = self.buffer[self.nextTranslate:self.bufferEnd]
+			self.bufferEnd = newEnd
+		else:
+			self.buffer = None
+			self.bufferSize = 0
+			self.bufferEnd = 0
+		self.nextTranslate = 0
+
 	def append(self, metacode):
 	
 		if self.bufferSize < (self.bufferEnd + len(metacode)):
 			# increment buffer size and copy into new array
-			print "growing metacode buffer"
 			diff = self.bufferEnd + len(metacode) - self.bufferSize
 			nblocks = diff/self.INCREMENT + 1
 			self.bufferSize = self.bufferSize + nblocks * self.INCREMENT
@@ -75,14 +98,64 @@ class GkiBuffer:
 			self.buffer = newbuffer
 		self.buffer[self.bufferEnd:self.bufferEnd+len(metacode)] = metacode
 		self.bufferEnd = self.bufferEnd + len(metacode)
-#		print 'gkibuffersize:',self.bufferSize,'gkibufconsize:',self.bufferEnd
-		
+
 	def get(self):
 	
 		if self.buffer:
 			return self.buffer[0:self.bufferEnd]
 		else:
 			return self.buffer
+
+	def getNextCode(self):
+		"""Read next opcode and argument from buffer, returning a tuple
+		with (opcode, arg).  Skips no-op codes and illegal codes.
+		Returns (None,None) on end of buffer or when opcode is truncated."""
+		ip = self.nextTranslate
+		lenMC = self.bufferEnd
+		while ip < lenMC:
+			if self.buffer[ip] == NOP:
+				ip = ip+1
+			elif self.buffer[ip] != BOI:
+				print "WARNING: missynched graphics data stream"
+				# find next possible beginning of instruction
+				ip = ip + 1
+				while ip < lenMC:
+					if self.buffer[ip] == BOI: break
+					ip = ip + 1
+				else:
+					# Unable to resync
+					print "WARNING: unable to resynchronize in graphics data stream"
+					break
+			else:
+				if ip+2 >= lenMC: break
+				opcode = self.buffer[ip+1]
+				arglen = self.buffer[ip+2]
+				if (ip+arglen) > lenMC: break
+				arg = self.buffer[ip+3:ip+arglen]
+				ip = ip + arglen
+				if ((opcode < 0) or 
+					(opcode > GKI_MAX_OP_CODE) or
+					(opcode in GKI_ILLEGAL_LIST)):
+					print "WARNING: Illegal graphics opcode = ",opcode
+				else:
+					# normal return
+					self.nextTranslate = ip
+					return (opcode, arg)
+		# end-of-buffer return
+		self.nextTranslate = ip
+		return (None, None)
+
+	def __len__(self):
+		return self.bufferEnd
+
+	def __getitem__(self,i):
+		if i >= self.bufferEnd:
+			raise IndexError("buffer index out of range")
+		return self.buffer[i]
+
+	def __getslice__(self,i,j):
+		if j > self.bufferEnd: j = self.bufferEnd
+		return self.buffer[i:j]
 
 class GkiKernel:
 
@@ -105,30 +178,51 @@ class GkiKernel:
 	
 		# if input parameter is string, assume it is a filename and read into
 		# a numeric array
-		if type(gkiMetacode) == StringType:
-			gwm.g.activeWindow.gwidget.iplot.gkiBuffer.append(
-				getdata(gkiMetacode))
-		else:
-			win = gwm.g.activeWindow
 		try:
-			win.gwidget.iplot.gkiBuffer.append(gkiMetacode)
+			if type(gkiMetacode) == StringType:
+				gkiMetacode = getdata(gkiMetacode)
+			win = gwm.getActiveWindow()
+			buffer = win.iplot.gkiBuffer
+			buffer.append(gkiMetacode)
 			# a hook for acting on the metacode, but is only a stub routine
 			# that must be overridden in the subclass (or not, depending)
-			self.translate(gkiMetacode, self.functionTable)
+			self.translate(buffer, self.functionTable)
 		except AttributeError:
-			print "Error, no IRAF plot window active"
-			
-	def translate(self, gkiMetacode, functionTable):
+			print "ERROR: no IRAF plot window active"
+
+	def translate(self, gkiBuffer, functionTable):
 
 		# stub routine to be overridden in subclass of GkiKernel
 		pass
+
+def gkiTranslate(metacode, functionTable):
+
+	"""General Function that can be used for decoding and interpreting
+	the GKI metacode stream. FunctionTable is a 28 element list containing
+	the functions to invoke for each opcode encountered. This table should
+	be different for each kernel that uses this function and the control
+	method.
+	This may be called with either a gkiBuffer or a simple numerical
+	array.  If a gkiBuffer, it translates only the previously untranslated
+	part of the gkiBuffer and updates the nextTranslate pointer."""
+
+	if isinstance(metacode, GkiBuffer):
+		gkiBuffer = metacode
+	else:
+		gkiBuffer = GkiBuffer(metacode)
+
+	opcode, arg = gkiBuffer.getNextCode()
+	while opcode != None:
+		apply(functionTable[opcode], (arg,))
+		opcode, arg = gkiBuffer.getNextCode()
+
 
 #********************************
 
 def getdata(filename):
 
 	f = open(filename, 'rb')
-	rawbytes = f.read(10000000l) 
+	rawbytes = f.read() 
 	rawarray = Numeric.fromstring(rawbytes,'s')
 	if BYTESWAP:
 		# intel for example
@@ -146,46 +240,3 @@ def ndcpairs(intarr):
 	f = ndc(intarr)
 	return f[0::2],f[1::2]
 
-def gkiTranslate(gkiMetacode, functionTable):
-
-	"""General Function that can be used for decoding and interpreting
-	the GKI metacode stream. FunctionTable is a 28 element list containing
-	the functions to invoke for each opcode encountered. This table should
-	be different for each kernel that uses this function and the control
-	method"""
-
-	# It is not expected that any metacode recieved from the iraf task
-	# will have incomplete gki instructions (i.e., continued in the
-	# next bunch of data from the iraf channel) but checks are performed
-	# here in case that assumption is not true and a message to
-	# that effect is printed out
-	
-	# initialize "instruction pointer"
-	ip = 0
-	lenMC = len(gkiMetacode)
-	# start interpreting instructions
-	while ip < lenMC:
-		if gkiMetacode[ip] == NOP:
-			ip = ip+1
-		elif gkiMetacode[ip] != BOI:
-			print "WARNING, missynched gki data stream"
-			# find next possible beginning of instruction
-			while gkiMetacode[ip] != BOI:
-				if ip >= lenMC:
-					print "Truncated GKI metacode?"
-					return
-				ip = ip+1
-		else:
-			if ip+2 >= lenMC:
-				print "Truncated GKI metacode"
-			opcode = gkiMetacode[ip+1]
-			if ((opcode < 0) or 
-				(opcode > GKI_MAX_OP_CODE) or
-				(opcode in [21,22,23,24])):
-					print "Illegal opcode found, opcode = ",opcode
-			arglen = gkiMetacode[ip+2]
-			if (ip+arglen) > lenMC:
-				print "Truncated GKI metacode"
-			arg = gkiMetacode[ip+3:ip+3+arglen-3]
-			apply(functionTable[opcode], (arg,))
-			ip = ip + arglen
