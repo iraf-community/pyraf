07a935d4577f502bc661a763277aabe6aea5a0f9
perry <perry@stsci.edu>
1999-04-21 17:39:39 +0000

    Window manipulation utilities for functionality not found in Tkinter: cursor warping, getting python window id, and setting focus to arbitrary non-Tk windows. Uses C extension module if present, routines do nothing in effect if not present.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@78 05fa7843-0683-0410-9e38-dfee850dc5be

---
+++ wutil.py
@@ -0,0 +1,13 @@
+"""Contains python routines to do special Window manipulations not
+possible in Tkinter.
+These are python stubs that are overloaded by a c version implementations.
+If the c versions do not exist, then these routines will do nothing"""
+
+def getWindowID(): return None
+def moveCursorTo(XWindowID, x, y): pass
+def setFocusTo(XWindowID): pass
+
+try:
+	from xlibtricks import *
+except ImportError:
+	pass
---
+++ xlibtricks.c
@@ -0,0 +1,84 @@
+#include <Python.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <stdio.h>
+
+/* Windows and cursor manipulations not provided by Tkinter or any other
+** standard python library
+*/
+
+void moveCursorTo(int win, int x, int y) {
+  Display *d;
+  Window w;
+  Display *XOpenDisplay(char *);
+  int s;
+  d = XOpenDisplay("eclipse.stsci.edu:0");
+  if (d == NULL)
+    printf("could not open display!\n");
+  w = (Window) win;
+  s = XGrabPointer(d,w,True,ButtonPressMask | EnterWindowMask,GrabModeSync,
+	GrabModeSync,None,None,CurrentTime);
+  XWarpPointer(d,None,w,0,0,0,0,x,y);
+  XUngrabPointer(d,CurrentTime);
+  XFlush(d);
+}
+
+PyObject *wrap_moveCursorTo(PyObject *self, PyObject *args) {
+  int x, y, w;
+  if (!PyArg_ParseTuple(args,"iii:warp",&w, &x, &y))
+    return NULL;
+  moveCursorTo(w,x,y);
+  return Py_None; 
+}
+
+int getWindowID() {
+   Display *d;
+   Window w;
+   int revert;
+   Display *XOpenDisplay(char *);
+   d = XOpenDisplay(NULL);
+   if (d == NULL)
+     printf("could not open display!\n");
+   XGetInputFocus(d,&w,&revert);
+   return (int) w;
+}
+
+PyObject *wrap_getWindowID(PyObject *self, PyObject *args) {
+  int result;
+  result = getWindowID();
+  return Py_BuildValue("i",result);
+}
+
+void setFocusTo(int win) {
+  Window w;
+  Display *d;   
+  Display *XOpenDisplay(char *);
+  w = (Window) win;
+  d = XOpenDisplay(NULL);
+  if (d == NULL)
+    printf("could not open display!\n");
+  XSetInputFocus(d,w,0,CurrentTime);
+  XFlush(d);
+}
+
+PyObject *wrap_setFocusTo(PyObject *self, PyObject *args) {
+  int win;
+  if (!PyArg_ParseTuple(args,"i:warp", &win))
+    return NULL;
+  setFocusTo(win);
+  return Py_None;
+}
+
+static PyMethodDef xlibtricksMethods[] = {
+  { "moveCursorTo",wrap_moveCursorTo, 1},
+  { "getWindowID",wrap_getWindowID, 1},
+  { "setFocusTo",wrap_setFocusTo, 1},
+  {NULL, NULL}
+};
+
+void initxlibtricks() {
+  PyObject *m;
+  m = Py_InitModule("xlibtricks", xlibtricksMethods);
+}
