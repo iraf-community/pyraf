e67fc4b0057134b25d6ce005630d6796f458e958
perry <perry@stsci.edu>
1999-05-25 20:29:01 +0000

    Added new functions to support focus management including getting window attributes, finding the top level window ID, etc.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@108 05fa7843-0683-0410-9e38-dfee850dc5be

--- wutil.py
+++ wutil.py
@@ -6,11 +6,58 @@ If the c versions do not exist, then these routines will do nothing
 $Id$
 """
 
+import struct, fcntl, sys
+
 def getWindowID(): return None
-def moveCursorTo(XWindowID, x, y): pass
-def setFocusTo(XWindowID): pass
+def moveCursorTo(WindowID, x, y): pass
+def setFocusTo(WindowID): pass
+def setBackingStore(WindowID): pass
+def getPointerPosition(WindowID): pass
+def getWindowAttributes(WindowID): pass
+def getParentID(WindowID): pass
 
 try:
 	from xlibtricks import *
 except ImportError:
 	pass
+
+terminalWindowID = getWindowID()
+
+def getTopID(WindowID):
+
+	"""Find top level X windows ID parent of given window.
+	If window is already top (or not implemented), it returns its own ID"""
+	wid = WindowID
+	while 1:
+		pid = getParentID(wid)
+		if not pid:
+			return wid
+		else:
+			wid = pid
+
+def isViewable(WindowID):
+
+	attrdict = getWindowAttributes(WindowID)
+	return attrdict['viewable']
+
+def getTermWindowSize():
+
+	"""return a tuple containing the x,y size of the terminal window
+	in characters"""
+
+	try:
+		import IOCTL
+		magicConstant = IOCTL.TIOCGWINSZ
+	except ImportError:
+		magicConstant = ord('T')*256 + 104 # at least on Solaris!
+	# define string to serve as memory area to recieve copy of structure
+	# created by IOCTL call
+	tstruct = ' '*20 # that should be more than enough memory
+	# xxx exception handling needed (but what exception to catch?)
+	rstruct = fcntl.ioctl(sys.stdin.fileno(), magicConstant, tstruct)
+	xsize, ysize = struct.unpack('hh',rstruct[0:4])
+	return xsize, ysize
+		
+
+# XXXX find more portable scheme for handling absence of FCNTL
+
--- xlibtricks.c
+++ xlibtricks.c
@@ -1,4 +1,5 @@
 #include <Python.h>
+#include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xos.h>
@@ -17,19 +18,22 @@ void moveCursorTo(int win, int x, int y) {
   Display *XOpenDisplay(char *);
   int s;
   d = XOpenDisplay(NULL);
-  if (d == NULL)
+  if (d == NULL) {
     printf("could not open display!\n");
+    return; 
+  }
   w = (Window) win;
   s = XGrabPointer(d,w,True,ButtonPressMask | EnterWindowMask,GrabModeSync,
 	GrabModeSync,None,None,CurrentTime);
   XWarpPointer(d,None,w,0,0,0,0,x,y);
   XUngrabPointer(d,CurrentTime);
   XFlush(d);
+  XCloseDisplay(d);
 }
 
 PyObject *wrap_moveCursorTo(PyObject *self, PyObject *args) {
   int x, y, w;
-  if (!PyArg_ParseTuple(args,"iii:warp",&w, &x, &y))
+  if (!PyArg_ParseTuple(args,"iii",&w, &x, &y))
     return NULL;
   moveCursorTo(w,x,y);
   return Py_None; 
@@ -41,9 +45,13 @@ int getWindowID() {
    int revert;
    Display *XOpenDisplay(char *);
    d = XOpenDisplay(NULL);
-   if (d == NULL)
+   if (d == NULL) {
      printf("could not open display!\n");
+     return -1;
+	}
    XGetInputFocus(d,&w,&revert);
+   XFlush(d);
+   XCloseDisplay(d);
    return (int) w;
 }
 
@@ -59,24 +67,159 @@ void setFocusTo(int win) {
   Display *XOpenDisplay(char *);
   w = (Window) win;
   d = XOpenDisplay(NULL);
-  if (d == NULL)
+  if (d == NULL) {
     printf("could not open display!\n");
+    return;
+  }
   XSetInputFocus(d,w,0,CurrentTime);
   XFlush(d);
+  XCloseDisplay(d);
 }
 
 PyObject *wrap_setFocusTo(PyObject *self, PyObject *args) {
   int win;
-  if (!PyArg_ParseTuple(args,"i:warp", &win))
+  if (!PyArg_ParseTuple(args,"i", &win))
     return NULL;
   setFocusTo(win);
   return Py_None;
+}
+
+void setBackingStore(int win) {
+  Window w;
+  XWindowAttributes wa;
+  XSetWindowAttributes  NewWinAttributes;
+  Display *d;
+  Display *XOpenDisplay(char *);
+  Status XGetWindowAttributes(Display *, Window, XWindowAttributes *);
+  int XChangeWindowAttributes(Display *, Window, unsigned long, 
+			       XSetWindowAttributes *);
+  w = (Window) win;
+  d = XOpenDisplay(NULL);
+  if (d == NULL) {
+    printf("could not open display!\n");
+    return;
+  }
+  XGetWindowAttributes(d, w, &wa);
+  if (XDoesBackingStore(wa.screen) != NotUseful) {
+    NewWinAttributes.backing_store = Always;
+    XChangeWindowAttributes(d, w, CWBackingStore, &NewWinAttributes);
+  }
+  XFlush(d);
+  XCloseDisplay(d);
+}
+
+PyObject *wrap_setBackingStore(PyObject *self, PyObject *args) {
+  int win;
+  if (!PyArg_ParseTuple(args,"i", &win))
+    return NULL;
+  setBackingStore(win);
+  return Py_None;
+}
+
+void getWindowAttributes(int win, XWindowAttributes *winAttr) {
+  Window w;
+  Display *d;
+  Display *XOpenDisplay(char *);
+  Status XGetWindowAttributes(Display *, Window, XWindowAttributes *);
+  w = (Window) win;
+  d = XOpenDisplay(NULL);
+  if (d == NULL) {
+    printf("could not open display!\n");
+    return;
+  }
+  XGetWindowAttributes(d, w, winAttr);
+  XCloseDisplay(d);
+}
+
+PyObject *wrap_getWindowAttributes(PyObject *self, PyObject *args) {
+	int win, viewable;
+	XWindowAttributes wa;
+	if (!PyArg_ParseTuple(args,"i", &win))
+		return NULL;
+	getWindowAttributes(win, &wa);
+	if (wa.map_state == IsViewable) {
+	  viewable = 1;
+	} else {
+	  viewable = 0;
+	}
+	return Py_BuildValue("{s:i,s:i,s:i,s:i,s:i,s:i,s:i}",
+		"x", wa.x,
+		"y", wa.y,
+		"rootID", (int) wa.root,
+		"width", wa.width,
+		"height", wa.height,
+		"borderWidth", wa.border_width,
+		"viewable", viewable);
+}
+
+PyObject *wrap_getPointerPosition(PyObject *self, PyObject *args) {
+	int win;  
+	Window w;
+	Bool inScreen;
+	Window root, child;
+	int root_x, root_y, win_x, win_y;
+	unsigned int mask;
+	Display *d;
+	Display *XOpenDisplay(char *);
+	Bool XQueryPointer(Display *, Window, Window *, Window *,
+		int *, int *, int *, int *, unsigned int *);	
+	if (!PyArg_ParseTuple(args,"i", &win))
+		return NULL;
+	w = (Window) win;
+	d = XOpenDisplay(NULL);
+	if (d == NULL) {
+		printf("could not open display!\n");
+		return;
+	}
+	inScreen = XQueryPointer(d, w, &root, &child, &root_x, &root_y,
+		&win_x, &win_y, &mask);
+	XCloseDisplay(d);
+	return Py_BuildValue("{s:i,s:i,s:i,s:i,s:i,s:i,s:i}",
+		"inScreen", (int) inScreen,
+		"rootID", (int) root,
+		"childID", (int) child,
+		"root_x", root_x,
+		"root_y", root_y,
+		"win_x", win_x,
+		"win_y", win_y);
+}
+
+PyObject *wrap_getParentID(PyObject *self, PyObject *args) {
+
+	int win;  
+	Window w;
+	Window root, parent, *children;
+	unsigned int nchildren;
+	Display *d;
+	Display *XOpenDisplay(char *);
+	Status XQueryTree(Display *, Window, Window *, Window *,
+		Window **, unsigned int *);
+	if (!PyArg_ParseTuple(args,"i", &win))
+		return NULL;
+	w = (Window) win;
+	d = XOpenDisplay(NULL);
+	if (d == NULL) {
+		printf("could not open display!\n");
+		return;
+	}
+	XQueryTree(d, w, &root, &parent, &children, &nchildren);
+	XFree(children);
+	XCloseDisplay(d);
+	if (root == parent) {
+	  return Py_None;
+	} else {
+	  return Py_BuildValue("i", (int) parent);
+	}
 }
 
 static PyMethodDef xlibtricksMethods[] = {
   { "moveCursorTo",wrap_moveCursorTo, 1},
   { "getWindowID",wrap_getWindowID, 1},
   { "setFocusTo",wrap_setFocusTo, 1},
+  { "setBackingStore",wrap_setBackingStore, 1},
+  { "getWindowAttributes",wrap_getWindowAttributes, 1},
+  { "getPointerPosition",wrap_getPointerPosition, 1},
+  { "getParentID",wrap_getParentID, 1},
   {NULL, NULL}
 };
 
