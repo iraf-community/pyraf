f359b02d87af8535374cb5f8f498d35d41f30677
rlw <rlw@stsci.edu>
2000-02-09 20:27:11 +0000

    Use threads (if available) to update Tk graphics windows while waiting for imcur input.  Also now correctly recognizes ^D character as EOF during imcur.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@248 05fa7843-0683-0410-9e38-dfee850dc5be

--- irafimcur.py
+++ irafimcur.py
@@ -10,7 +10,11 @@ $Id$
 import os, iraf, wutil
 try:
 	import cdl
-	def imcur(): return  _imcur()
+	try:
+		import threading
+		def imcur(): return  _imcur(_threadedReadCursor)
+	except ImportError:
+		def imcur(): return  _imcur()
 except ImportError:
 	def imcur():
 		raise iraf.IrafError(
@@ -18,7 +22,37 @@ except ImportError:
 
 prevDisplayHandle = None
 
-def _imcur():
+def _readCursor(displayHandle, retlist=None):
+	"""Reads image cursor and returns tuple with key, position"""
+	# Require keystroke to read cursor position (0 arg)
+	rv = cdl.cdl_readCursor(displayHandle, 0)
+	if retlist is not None: retlist.append(rv)
+	if iraf.Verbose>1: print rv
+	return rv
+
+def _threadedReadCursor(displayHandle):
+	"""Reads image cursor in a thread so Tk windows can remain active"""
+	result = []
+	th = threading.Thread(target=_readCursor, args=(displayHandle, result))
+	if iraf.Verbose>1: print "starting imcur thread"
+	th.start()
+	timeout = 0.5
+	# messy -- I wish I could just sleep until thread is done, letting
+	# Python's implicit mainloop run.  That doesn't work though.
+	import gwm
+	win = gwm.getActiveWindowTop()
+	while th.isAlive():
+		th.join(timeout)
+		if win is None:
+			win = gwm.getActiveWindowTop()
+			if win is not None: win.update()
+		else:
+			win.update()
+	if iraf.Verbose>1: print "finished imcur thread"
+	return result[0]
+
+
+def _imcur(readCursor=_readCursor):
 
 	"""Returns the string expected for IRAF's imcur parameter"""
 
@@ -28,16 +62,17 @@ def _imcur():
 		displayHandle = imageDisplay.getHandle()
 #		imageDisplay.activateImcur()
 		wutil.focusController.setFocusTo("image")
-		# Require keystroke to read cursor position (0 arg)
-		key, xpos, ypos, dummy = cdl.cdl_readCursor(displayHandle, 0)
+		# Read cursor position at keystroke
+		key, xpos, ypos, dummy = readCursor(displayHandle)
 		if not imageDisplay.getWindowID():
 			imageDisplay.setWindowID()
 		frame = cdl.cdl_getFrame(displayHandle)
 		# don't close the display!
-		if key == '\\':
-			# This is what cdl returns for any control key. Since
-			# we need to trap control-D, this is what we will
-			# interpret it as.
+		if key in ['\\', '\000']:
+			# This is what cdl returns for any control key. (It seems
+			# to return '\\' for everything except ^D and ^Z, which return a
+			# null character.) Since we need to trap control-D, this is what
+			# we will interpret all control characters as EOF.
 			key = ' ' # just to give it a value
 			raise EOFError # irafexecute will handle this properly
 		if key == ':':
@@ -66,4 +101,3 @@ def _imcur():
 		raise
 	wutil.focusController.restoreLast()
 	return "%f %f %d %s %s" % (xpos, ypos, wcs, key, colonString)
-	
