08f4d8a12d5d25bc9cd5ae40e82bd0bb3ae0eb85
rlw <rlw@stsci.edu>
2000-02-23 21:35:40 +0000

    New files pyraf, __init__.py as part of reorganization into a Python package
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@269 05fa7843-0683-0410-9e38-dfee850dc5be

---
+++ __init__.py
@@ -0,0 +1,89 @@
+"""__init__.py: main Pyraf package initialization
+
+Checks sys.argv[0] == 'pyraf' to determine whether IRAF initialization
+is done verbosely or quietly.
+
+$Id$
+
+R. White, 2000 February 18
+"""
+
+import os, sys, __main__
+
+def usage():
+	print __main__.__doc__
+	sys.stdout.flush()
+	sys.exit()
+
+# set search path to include current directory
+
+if "." not in sys.path: sys.path.insert(0, ".")
+
+# Grab the terminal window's id at the earliest possible moment
+import wutil
+
+# Modify the standard import mechanism to make it more
+# convenient for the iraf module
+import irafimport
+
+# this gives more useful tracebacks for CL scripts
+import cllinecache
+
+# Set clean name strategy
+import irafnames
+irafnames.setCleanStrategy()
+
+# now get ready to do the serious IRAF initialization
+
+import iraf
+
+_pname = os.path.split(sys.argv[0])[1]
+if _pname != "pyraf":
+	# if not executing as pyraf main, just initialize iraf module
+	# quietly load initial iraf symbols and packages
+	iraf.Init(doprint=0, hush=1)
+else:
+	# special initialization when this is the main program
+	# read the user's startup file (if there is one)
+	if os.environ.has_key("PYTHONSTARTUP") and \
+			os.path.isfile(os.environ["PYTHONSTARTUP"]):
+		execfile(os.environ["PYTHONSTARTUP"])
+
+	# command-line options
+
+	import getopt
+	try:
+		optlist, args = getopt.getopt(sys.argv[1:], "imvh")
+		if len(args) > 1:
+			print 'Error: more than one savefile argument'
+			usage()
+	except getopt.error, e:
+		print str(e)
+		usage()
+	verbose = 0
+	doCmdline = 1
+	if optlist:
+		for opt, value in optlist:
+			if opt == "-i":
+				doCmdline = 0
+			elif opt == "-m":
+				doCmdline = 1
+			elif opt == "-v":
+				verbose = verbose + 1
+			elif opt == "-h":
+				usage()
+			else:
+				print "Program bug, uninterpreted option", opt
+				raise SystemExit
+	iraf.setVerbose(verbose)
+	del getopt, verbose, usage, optlist
+
+	# load initial iraf symbols and packages
+	if args:
+		iraf.Init(savefile=args[0])
+	else:
+		iraf.Init()
+	del args
+
+help = iraf.help
+
