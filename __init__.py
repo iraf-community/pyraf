4df22b173e53bc8ae6b5078e11b8b78e0570e82f
rlw <rlw@stsci.edu>
2001-11-30 20:58:03 +0000

    __init__: - Move __version__ from pyraf file to this module - Set __version__ to v0.9 (2001Nov30) pyraf, pyraf.in: - Get __version__ from the pyraf (__init__.py) module - Minor cleanup edits
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@465 05fa7843-0683-0410-9e38-dfee850dc5be

--- __init__.py
+++ __init__.py
@@ -7,13 +7,14 @@ $Id$
 
 R. White, 2000 February 18
 """
+__version__ = "v0.9 (2001Nov30)"
 
 import os, sys, __main__
 
 def usage():
-	print __main__.__doc__
-	sys.stdout.flush()
-	sys.exit()
+    print __main__.__doc__
+    sys.stdout.flush()
+    sys.exit()
 
 # set search path to include current directory
 
@@ -37,10 +38,10 @@ irafnames.setCleanStrategy()
 import irafexecute, clcache
 _oldexitfunc = getattr(sys, 'exitfunc', None)
 def _cleanup(last_exit = _oldexitfunc):
-	iraf.gflush()
-	del irafexecute.processCache
-	del clcache.codeCache
-	if last_exit: last_exit()
+    iraf.gflush()
+    del irafexecute.processCache
+    del clcache.codeCache
+    if last_exit: last_exit()
 sys.exitfunc = _cleanup
 
 # now get ready to do the serious IRAF initialization
@@ -49,47 +50,46 @@ import iraf
 
 _pname = os.path.split(sys.argv[0])[1]
 if _pname != "pyraf":
-	# if not executing as pyraf main, just initialize iraf module
-	# quietly load initial iraf symbols and packages
-	iraf.Init(doprint=0, hush=1)
+    # if not executing as pyraf main, just initialize iraf module
+    # quietly load initial iraf symbols and packages
+    iraf.Init(doprint=0, hush=1)
 else:
-	# special initialization when this is the main program
+    # special initialization when this is the main program
 
-	# command-line options
+    # command-line options
 
-	import getopt
-	try:
-		optlist, args = getopt.getopt(sys.argv[1:], "imvh")
-		if len(args) > 1:
-			print 'Error: more than one savefile argument'
-			usage()
-	except getopt.error, e:
-		print str(e)
-		usage()
-	verbose = 0
-	doCmdline = 1
-	if optlist:
-		for opt, value in optlist:
-			if opt == "-i":
-				doCmdline = 0
-			elif opt == "-m":
-				doCmdline = 1
-			elif opt == "-v":
-				verbose = verbose + 1
-			elif opt == "-h":
-				usage()
-			else:
-				print "Program bug, uninterpreted option", opt
-				raise SystemExit
-	iraf.setVerbose(verbose)
-	del getopt, verbose, usage, optlist
+    import getopt
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], "imvh")
+        if len(args) > 1:
+            print 'Error: more than one savefile argument'
+            usage()
+    except getopt.error, e:
+        print str(e)
+        usage()
+    verbose = 0
+    doCmdline = 1
+    if optlist:
+        for opt, value in optlist:
+            if opt == "-i":
+                doCmdline = 0
+            elif opt == "-m":
+                doCmdline = 1
+            elif opt == "-v":
+                verbose = verbose + 1
+            elif opt == "-h":
+                usage()
+            else:
+                print "Program bug, uninterpreted option", opt
+                raise SystemExit
+    iraf.setVerbose(verbose)
+    del getopt, verbose, usage, optlist
 
-	# load initial iraf symbols and packages
-	if args:
-		iraf.Init(savefile=args[0])
-	else:
-		iraf.Init()
-	del args
+    # load initial iraf symbols and packages
+    if args:
+        iraf.Init(savefile=args[0])
+    else:
+        iraf.Init()
+    del args
 
 help = iraf.help
-
