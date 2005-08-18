94e00f81c6667b2a63c11d8e8f4a4cf06bb48ebe
nadia@astropy.scipy.org <nadia@astropy.scipy.org@stsci.edu>
2005-08-18 19:38:39 +0000

    Modified X11 libs search path to support 64 bit linux. Removed reference to CVS specific files and added .svn to the list of ignored directories.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@681 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -7,14 +7,27 @@ from distutils.sysconfig import *
 from distutils.command.install import install
 
 
+#local_libs = parse_makefile(get_makefile_filename())['LOCALMODLIBS']
+py_includes = get_python_inc(plat_specific=1)
+py_libs =  get_python_lib(plat_specific=1, standard_lib = 1)
+#py_bin = parse_makefile(get_makefile_filename())['BINDIR']
+x_libraries = 'X11'
+#scripts = parse_makefile(get_makefile_filename())['SCRIPTDIR']
+ver = sys.version_info
+python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
+add_lib_dirs = []
+add_inc_dirs = []
+add_inc_dirs.append(py_includes)
+add_lib_dirs.append(py_libs)
 
 def find_x(xdir=""):
     if xdir != "":
-        x_lib_dirs = os.path.join(xdir,'lib')
-        x_inc_dirs = os.path.join(xdir,'include')
+        add_lib_dirs.append(os.path.join(xdir,'lib'))
+        add_inc_dirs.append(os.path.join(xdir,'include'))
     elif sys.platform == 'darwin' or sys.platform.startswith('linux'):
-        x_lib_dirs = '/usr/X11R6/lib'
-        x_inc_dirs = '/usr/X11R6/include'
+        add_lib_dirs.append('/usr/X11R6/lib64')
+        add_lib_dirs.append('/usr/X11R6/lib')
+        add_inc_dirs.append('/usr/X11R6/include')
     else:
         try:
             import Tkinter
@@ -35,45 +48,40 @@ def find_x(xdir=""):
                 for lib in lib_list:
                     if string.find(lib, 'libX11') == 0:
                         ind = lib_list.index(lib)
-                        x_lib_dirs = os.path.dirname(lib_list[ind + 2])
+                        add_lib_dirs.append(os.path.dirname(lib_list[ind + 2]))
                         break
-                x_inc_dirs = os.path.join(x_lib_dirs, '../include')
+                add_inc_dirs.append(os.path.join(x_lib_dirs, '../include'))
         except:
             raise ImportError("Tkinter is not installed")
-    return x_lib_dirs, x_inc_dirs
-
-#local_libs = parse_makefile(get_makefile_filename())['LOCALMODLIBS']
-py_includes = get_python_inc(plat_specific=1)
-py_libs =  get_python_lib(plat_specific=1, standard_lib = 1)
-py_bin = parse_makefile(get_makefile_filename())['BINDIR']
-x_libraries = 'X11'
-#scripts = parse_makefile(get_makefile_filename())['SCRIPTDIR']
-ver = sys.version_info
-python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
 
 
-def getExtensions(args, x_inc_dirs, x_lib_dirs):
+def getExtensions(args, add_inc_dirs, add_lib_dirs):
     for a in args:
         if string.find(a, '--with-opengl=') != 0:
             e1 = [Extension('pyraf/sscanfmodule', ['src/sscanfmodule.c'],
-                           include_dirs=[py_includes]),
+                           include_dirs=add_inc_dirs),
                   Extension('pyraf/xutilmodule', ['src/xutil.c'],
-                           include_dirs=[py_includes, x_inc_dirs],
-                           library_dirs=[x_lib_dirs],
+                           include_dirs=add_inc_dirs,
+                           library_dirs=add_lib_dirs,
                            libraries = [x_libraries])]
         else:
             opengl_inc=string.split(a, '=')[1]
+            add_inc_dirs.append(opengl_inc)
+            add_inc_dirs.append(tcl_inc)
+            add_inc_dirs.append(src)
+            add_lib_dirs.append(tcl_lib)
             sys.argv.remove(a)
             e1 = [Extension('pyraf/sscanfmodule', ['src/sscanfmodule.c'],
-                           include_dirs=[py_includes]),
+                           include_dirs=add_inc_dirs),
                   Extension('pyraf/xutilmodule', ['src/xutil.c'],
-                           include_dirs=[py_includes, x_inc_dirs],
-                           library_dirs=[x_lib_dirs],
+                           include_dirs=add_inc_dirs,
+                           library_dirs=add_lib_dirs,
                            libraries = [x_libraries]),
                   Extension('pyraf/toglcolorsmodule', ['src/toglcolors.c'],
-                           include_dirs=[py_includes, x_inc_dirs, 'src', opengl_inc, tcl_inc],
-                           library_dirs=[x_lib_dirs, tcl_lib],
-                           extra_objects = [os.path.join(tcl_lib, "Togl.so")])]
+                           include_dirs=add_inc_dirs,
+                           library_dirs=add_lib_dirs,
+                           extra_objects = [os.path.join(tcl_lib, "Togl.so")],
+                            )]
 
     return e1
 
@@ -81,13 +89,13 @@ def getExtensions(args, x_inc_dirs, x_lib_dirs):
 def getDataDir(args):
     for a in args:
         if string.find(a, '--home=') == 0:
-            dir = string.split(a, '=')[1]
+            dir = os.path.abspath(string.split(a, '=')[1])
             data_dir = os.path.join(dir, 'lib/python/pyraf')
         elif string.find(a, '--prefix=') == 0:
-            dir = string.split(a, '=')[1]
+            dir = os.path.abspath(string.split(a, '=')[1])
             data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/pyraf')
         elif a.startswith('--install-data='):
-            dir = string.split(a, '=')[1]
+            dir = os.path.abspath(string.split(a, '=')[1])
             data_dir = dir
         else:
             data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/pyraf')
@@ -102,7 +110,8 @@ def dolocal():
         print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir> --install-headers=<install-dir>/pyraf --install-scripts=<install-dir>/pyraf --install-data=<install-dir>/pyraf"
     for a in sys.argv:
         if a.startswith("--local="):
-            dir = a.split("=")[1]
+            dir = os.path.abspath(a.split("=")[1])
+            print 'dir ', dir
             sys.argv.extend([
                 "--install-lib="+dir,
                 "--install-headers="+os.path.join(dir,"pyraf"),
@@ -112,7 +121,7 @@ def dolocal():
             sys.argv.remove(a)
 
 
-def dosetup(x_lib_dirs, x_inc_dirs, data_dir, ext):
+def dosetup(data_dir, ext):
     r = setup(name = "PyRAF",
      version = "1.1",
      description = "A Python based CL for IRAF",
@@ -136,7 +145,7 @@ def copy_clcache(data_dir, args):
             shutil.rmtree(clcache_dir)
         os.mkdir(clcache_dir)
         for file in os.listdir('data/clcache'):
-            if fnmatch.fnmatch(file, 'CVS'):
+            if fnmatch.fnmatch(file, '.svn'):
                 pass
             else:
                 shutil.copy2(os.path.join('data/clcache', file), clcache_dir)
@@ -149,12 +158,13 @@ def main():
         if a.startswith('--with-x='):
             x_dir = a.split("=")[1]
             sys.argv.remove(a)
-    x_lib_dirs, x_inc_dirs = find_x(x_dir)
+    find_x(x_dir)
     dolocal()
-    print x_lib_dirs, x_inc_dirs
+    print add_lib_dirs, add_inc_dirs
     data_dir = getDataDir(args)
-    ext = getExtensions(args, x_inc_dirs, x_lib_dirs)
-    dosetup(x_lib_dirs, x_inc_dirs, data_dir, ext)
+
+    ext = getExtensions(args, add_inc_dirs, add_lib_dirs)
+    dosetup(data_dir, ext)
     copy_clcache(data_dir, args)
 
 
