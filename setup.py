4710f65242c935235114b88448fb0f1bb472c27b
nadia@astropy.scipy.org <nadia@astropy.scipy.org@stsci.edu>
2006-01-23 22:58:30 +0000

    Removed support for OpenGL
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@694 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, os.path, sys, shutil, commands, fnmatch
+import os, os.path, sys, shutil, commands, fnmatch, glob
 from distutils.core import setup,Extension
 from distutils.command.build_ext import build_ext
 from distutils.sysconfig import *
@@ -13,8 +13,19 @@ py_libs =  get_python_lib(plat_specific=1, standard_lib = 1)
 #py_bin = parse_makefile(get_makefile_filename())['BINDIR']
 x_libraries = 'X11'
 #scripts = parse_makefile(get_makefile_filename())['SCRIPTDIR']
-ver = sys.version_info
-python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
+#ver = sys.version_info
+#python_exec = 'python' + str(ver[0]) + '.' + str(ver[1])
+pythonlib = get_python_lib(plat_specific=1)
+pythoninc = get_python_inc()
+ver = get_python_version()
+pythonver = 'python' + ver
+data_dir = pythonlib
+
+PYRAF_DATA_FILES = ['data/blankcursor.xbm', 'data/epar.optionDB', 'data/pyraflogo_rgb_web.gif', 'lib/LICENSE.txt']
+
+PYRAF_CLCACHE = glob.glob(os.path.join('data', 'clcache', '*'))
+
+
 add_lib_dirs = []
 add_inc_dirs = []
 add_inc_dirs.append(py_includes)
@@ -56,73 +67,47 @@ def find_x(xdir=""):
 
 
 
-def getExtensions(args, add_inc_dirs, add_lib_dirs):
-    for a in args:
-        if string.find(a, '--with-opengl=') != 0:
-            e1 = [Extension('pyraf/sscanfmodule', ['src/sscanfmodule.c'],
-                           include_dirs=add_inc_dirs),
-                  Extension('pyraf/xutilmodule', ['src/xutil.c'],
-                           include_dirs=add_inc_dirs,
-                           library_dirs=add_lib_dirs,
-                           libraries = [x_libraries])]
-        else:
-            opengl_inc=string.split(a, '=')[1]
-            add_inc_dirs.append(opengl_inc)
-            add_inc_dirs.append(tcl_inc)
-            add_inc_dirs.append(src)
-            add_lib_dirs.append(tcl_lib)
-            sys.argv.remove(a)
-            e1 = [Extension('pyraf/sscanfmodule', ['src/sscanfmodule.c'],
-                           include_dirs=add_inc_dirs),
-                  Extension('pyraf/xutilmodule', ['src/xutil.c'],
-                           include_dirs=add_inc_dirs,
-                           library_dirs=add_lib_dirs,
-                           libraries = [x_libraries]),
-                  Extension('pyraf/toglcolorsmodule', ['src/toglcolors.c'],
-                           include_dirs=add_inc_dirs,
-                           library_dirs=add_lib_dirs,
-                           extra_objects = [os.path.join(tcl_lib, "Togl.so")],
-                            )]
-
-    return e1
+PYRAF_EXTENSIONS = [Extension('pyraf/sscanfmodule', ['src/sscanfmodule.c'],
+                              include_dirs=add_inc_dirs),
+                    Extension('pyraf/xutilmodule', ['src/xutil.c'],
+                              include_dirs=add_inc_dirs,
+                              library_dirs=add_lib_dirs,
+                              libraries = [x_libraries])]
 
 
-def getDataDir(args):
-    for a in args:
-        if string.find(a, '--home=') == 0:
-            dir = os.path.abspath(string.split(a, '=')[1])
-            data_dir = os.path.join(dir, 'lib/python/pyraf')
-        elif string.find(a, '--prefix=') == 0:
-            dir = os.path.abspath(string.split(a, '=')[1])
-            data_dir = os.path.join(dir, 'lib', python_exec, 'site-packages/pyraf')
-        elif a.startswith('--install-data='):
-            dir = os.path.abspath(string.split(a, '=')[1])
-            data_dir = dir
-        else:
-            data_dir = os.path.join(sys.prefix, 'lib', python_exec, 'site-packages/pyraf')
-    return data_dir
-
-def dolocal():
-    """Adds a command line option --local=<install-dir> which is an abbreviation for
-    'put all of pyraf in <install-dir>/pyraf'."""
-    if "--help" in sys.argv:
-        print >>sys.stderr
-        print >>sys.stderr, " options:"
-        print >>sys.stderr, "--local=<install-dir>    same as --install-lib=<install-dir> --install-headers=<install-dir>/pyraf --install-scripts=<install-dir>/pyraf --install-data=<install-dir>/pyraf"
-    for a in sys.argv:
-        if a.startswith("--local="):
-            dir = os.path.abspath(a.split("=")[1])
-            print 'dir ', dir
-            sys.argv.extend([
+args = sys.argv
+for a in args:
+    if string.find(a, '--home=') == 0:
+        dir = os.path.abspath(string.split(a, '=')[1])
+        data_dir = os.path.join(dir, 'lib', 'python', 'pyraf')
+    elif string.find(a, '--prefix=') == 0:
+        dir = os.path.abspath(string.split(a, '=')[1])
+        data_dir = os.path.join(dir, 'lib', pythonver, 'site-packages', 'pyraf')
+    elif a.startswith('--install-data='):
+        dir = os.path.abspath(string.split(a, '=')[1])
+        data_dir = dir
+    elif a.startswith("--local="):
+         """Adds a command line option --local=<install-dir> which is an abbreviation for
+         'put all of pyraf in <install-dir>/pyraf'."""
+         dir = os.path.abspath(a.split("=")[1])
+         sys.argv.extend([
                 "--install-lib="+dir,
                 "--install-headers="+os.path.join(dir,"pyraf"),
-                "--install-scripts="+os.path.join(dir,"pyraf"),
+                "--install-scripts=%s" % os.path.join(dir,"pyraf"),
                 "--install-data="+os.path.join(dir,"pyraf")
                 ])
-            sys.argv.remove(a)
+         sys.argv.remove(a)
+    else:
+        data_dir = os.path.join(pythonlib, 'pyraf')
+
+    
+PYRAF_CLCACHE_DIR = os.path.join(data_dir, 'pyraf', 'clcache')
+DATA_FILES = [(data_dir, PYRAF_DATA_FILES), (PYRAF_CLCACHE_DIR, PYRAF_CLCACHE)]
+
+print 'args', sys.argv
 
 
-def dosetup(data_dir, ext):
+def dosetup():
     r = setup(name = "PyRAF",
      version = "1.2",
      description = "A Python based CL for IRAF",
@@ -133,40 +118,27 @@ def dosetup(data_dir, ext):
      platforms = ["unix"],
      packages = ['pyraf'],
      package_dir = {'pyraf':'lib'},
-     data_files = [(data_dir,['data/blankcursor.xbm']), (data_dir, ['data/epar.optionDB']), (data_dir,['data/pyraflogo_rgb_web.gif']), (data_dir,['lib/LICENSE.txt'])],
+     data_files = DATA_FILES,
      scripts = ['lib/pyraf'],
-     ext_modules = ext)
+     ext_modules = PYRAF_EXTENSIONS)
 
     return r
 
-def copy_clcache(data_dir, args):
-    if 'install' in args:
-        clcache_dir = os.path.join(data_dir,'clcache')
-        if os.path.exists(clcache_dir):
-            shutil.rmtree(clcache_dir)
-        os.mkdir(clcache_dir)
-        for file in os.listdir('data/clcache'):
-            if fnmatch.fnmatch(file, '.svn'):
-                pass
-            else:
-                shutil.copy2(os.path.join('data/clcache', file), clcache_dir)
-
 
 def main():
-    args = sys.argv
+    args = sys.argv[2:]
     x_dir = ""
     for a in args:
         if a.startswith('--with-x='):
             x_dir = a.split("=")[1]
             sys.argv.remove(a)
     find_x(x_dir)
-    dolocal()
+    #dolocal()
     print add_lib_dirs, add_inc_dirs
-    data_dir = getDataDir(args)
+    #data_dir = getDataDir(args)
 
-    ext = getExtensions(args, add_inc_dirs, add_lib_dirs)
-    dosetup(data_dir, ext)
-    copy_clcache(data_dir, args)
+    dosetup()
+    #copy_clcache(data_dir, args)
 
 
 if __name__ == "__main__":
