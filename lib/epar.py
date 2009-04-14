d2f10d7fad26d85dda28bcfcf2d2011254912ac8
sontag <sontag@stsci.edu>
2009-04-14 19:01:19 +0000

    for #87; updated epars help string
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@997 05fa7843-0683-0410-9e38-dfee850dc5be

--- lib/epar.py
+++ lib/epar.py
@@ -16,6 +16,138 @@ from pytools.irafglobals import IrafError
 import iraf, irafpar, irafhelp, wutil, pseteparoption
 from pyrafglobals import pyrafDir
 
+# tool help
+eparHelpString = """\
+The PyRAF Parameter Editor window is used to edit IRAF parameter sets.  It
+allows multiple parameter sets to be edited concurrently (e.g., to edit IRAF
+Psets).  It also allows the IRAF task help to be displayed in a separate window
+that remains accessible while the parameters are being edited.
+
+
+Editing Parameters
+--------------------
+
+Parameter values are modified using various GUI widgets that depend on the
+parameter properties.  It is possible to edit parameters using either the mouse
+or the keyboard.  Most parameters have a context-dependent menu accessible via
+right-clicking that enables unlearning the parameter (restoring its value to
+the task default), clearing the value, and activating a file browser that
+allows a filename to be selected and entered in the parameter field.  Some
+items on the right-click pop-up menu may be disabled depending on the parameter
+type (e.g., the file browser cannot be used for numeric parameters.)
+
+The mouse-editing behavior should be familiar, so the notes below focus on
+keyboard-editing.  When the editor starts, the first parameter is selected.  To
+select another parameter, use the Tab key (Shift-Tab to go backwards) or Return
+to move the focus from item to item. The Up and Down arrow keys also move
+between fields.  The toolbar buttons can also be selected with Tab.  Use the
+space bar to "push" buttons or activate menus.
+
+Enumerated Parameters
+        Parameters that have a list of choices use a drop-down menu.  The space
+        bar causes the menu to appear; once it is present, the up/down arrow
+        keys can be used to select different items.  Items in the list have
+        accelerators (underlined, generally the first letter) that can be typed
+        to jump directly to that item.  When editing is complete, hit Return or
+        Tab to accept the changes, or type Escape to close the menu without
+        changing the current parameter value.
+
+Boolean Parameters
+        Boolean parameters appear as Yes/No radio buttons.  Hitting the space
+        bar toggles the setting, while 'y' and 'n' can be typed to select the
+        desired value.
+
+Parameter Sets
+        Parameter sets (Psets) appear as a button which, when clicked, brings
+        up a new editor window.  Note that two (or more) parameter lists can be
+        edited concurrently.  The Package and Task identification are shown
+        in the window and in the title bar.
+
+Text Entry Fields
+        Strings, integers, floats, etc. appear as text-entry fields.  Values
+        are verified to to be legal before being stored in the parameter. If an
+        an attempt is made to set a parameter to an illegal value, the program
+        beeps and a warning message appears in the status bar at the bottom of
+        the window.
+
+        To see the value of a string that is longer than the entry widget,
+        either use the left mouse button to do a slow "scroll" through the
+        entry or use the middle mouse button to "pull" the value in the entry
+        back and forth quickly.  In either case, just click in the entry widget
+        with the mouse and then drag to the left or right.  If there is a
+        selection highlighted, the middle mouse button may paste it in when
+        clicked.  It may be necessary to click once with the left mouse
+        button to undo the selection before using the middle button.
+
+        You can also use the left and right arrow keys to scroll through the
+        selection.  Control-A jumps to the beginning of the entry, and
+        Control-E jumps to the end of the entry.
+
+
+The Menu Bar
+--------------
+
+File menu:
+    Execute
+             Save all the parameters, close the editor windows, and start the
+             IRAF task.  This is disabled in the secondary windows used to edit
+             Psets.
+    Save & Quit
+             Save the parameters and close the editor window.  The task is not
+             executed.
+    Save As...
+             Save the parameters to a user-specified file.  The task is not
+             executed.
+    Unlearn/Defaults
+             Restore all parameters to the system default values for this
+             task.  Note that individual parameters can be unlearned using the
+             menu shown by right-clicking on the parameter entry.
+    Cancel
+             Cancel editing session and exit the parameter editor.  Changes
+             that were made to the parameters are not saved; the parameters
+             retain the values they had when the editor was started.
+
+Open... menu:
+     Load parameters from any applicable user file found.  Values are not
+     stored unless Execute or Save is then selected.  If no such files are
+     found, this menu is not shown.
+
+Options menu:
+    Display Task Help in a Window
+             Help on the IRAF task is available through the Help menu.  If this
+             option is selected, the help text is displayed in a pop-up window.
+             This is the default behavior.
+    Display Task Help in a Browser
+             If this option is selected, instead of a pop-up window help is
+             displayed in the user's web browser.  This requires access to
+             the internet and is a somewhat experimental feature.  The HTML
+             version of help does have some nice features such as links to
+             other IRAF tasks.
+
+Help menu:
+    Task Help
+             Display help on the IRAF task whose parameters are being edited.
+             By default the help pops up in a new window, but the help can also
+             be displayed in a web browser by modifying the Options.
+    EPAR Help
+             Display this help.
+
+
+Toolbar Buttons
+-----------------
+
+The Toolbar contains a set of buttons that provide shortcuts for the most
+common menu bar actions.  Their names are the same as the menu items given
+above: Execute, Save & Quit, Defaults, Cancel, and Task Help.  The Execute
+button is disabled in the secondary windows used to edit Psets.
+
+Note that the toolbar buttons are accessible from the keyboard using the Tab
+and Shift-Tab keys.  They are located in sequence before the first parameter.
+If the first parameter is selected, Shift-Tab backs up to the "Task Help"
+button, and if the last parameter is selected then Tab wraps around and selects
+the "Execute" button.
+"""
+
 
 def epar(theTask, parent=None, isChild=0):
 
@@ -79,6 +211,7 @@ class PyrafEparDialog(editpar.EditParDialog):
         self._showExtraHelpButton = True
 
         self._appName             = "Epar"
+        self._appHelpString       = eparHelpString
         self._unpackagedTaskTitle = "Filename"
         self._defaultsButtonTitle = "Unlearn"
 
