a39472ebfec75bce23301208f02b70f89f330544
stsci_embray <stsci_embray@stsci.edu>
2011-06-20 19:33:31 +0000

    Moving scripts to a separate scripts directory
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/branches/setup_refactoring@1454 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 
-# We use the local copy of stsci_distutils_hack, unless
-# the user asks for the stpytools version
-from __future__ import division # confidence high
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-import os
-if os.getenv("USE_STPYTOOLS") :
-    import pytools.stsci_distutils_hack as H
-    pytools_version = "3.0"
-else :
-    import stsci_distutils_hack as H
-    pytools_version = None
-
-H.run(pytools_version = pytools_version)
-
-
+setup(
+    setup_requires=['d2to1>=0.2.2'],
+    d2to1=True
+)
