163b26878a6b041d3cd791a4cc23ab09859f2a65
stsci_embray <stsci_embray@stsci.edu>
2011-06-23 21:15:19 +0000

    Updated d2to1 version requirement
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/branches/setup_refactoring@1456 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ except ImportError:
     from setuptools import setup
 
 setup(
-    setup_requires=['d2to1>=0.2.2', 'stsci.distutils>=0.2dev'],
+    setup_requires=['d2to1>=0.2.3', 'stsci.distutils>=0.2dev'],
     d2to1=True
 )
