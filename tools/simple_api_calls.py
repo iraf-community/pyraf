782b5bb15aefe8d58bc41802f087a978c72fb2db
sontag <sontag@stsci.edu>
2011-09-23 16:02:34 +0000

    upd
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@1536 05fa7843-0683-0410-9e38-dfee850dc5be

--- tools/simple_api_calls.py
+++ tools/simple_api_calls.py
@@ -5,13 +5,15 @@ API for use in testing, starting with the very simplest of function calls.
 This test is useful in special installations, such as one without IRAF. """
 
 import sys
+print("1 - about to import iraf ...")
 from pyraf import iraf
+print("2 - about to import pyraf ...") # should be essentially a no-op
 import pyraf
-print("pwd is...")
-iraf.pwd() # will print
-print("ver is...")
+print("3 - ver is ...")
 print(pyraf.__version__)
-print("imheader of dev$pix is ... (requires IRAF)")
+print("4 - pwd is ...")
+iraf.pwd() # will print
+print("5 - imheader of dev$pix is ... (requires IRAF)")
 iraf.images(_doprint=0) # load images
 iraf.imutil(_doprint=0) # load imutil
 iraf.imheader("dev$pix")
