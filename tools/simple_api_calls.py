4bb86cbf8d37014820be1c7f318c8f94befdbdda
sontag <sontag@stsci.edu>
2011-10-19 16:29:40 +0000

    updated
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@1555 05fa7843-0683-0410-9e38-dfee850dc5be

--- tools/simple_api_calls.py
+++ tools/simple_api_calls.py
@@ -5,25 +5,28 @@ API for use in testing, starting with the very simplest of function calls.
 This test is useful in special installations, such as one without IRAF. """
 
 import sys
-print("simple #1 - about to: from pyraf import iraf ...")
+print("Simple #1 - about to: from pyraf import iraf ...")
 from pyraf import iraf
-print("simple #2 - about to import pyraf ...") # should be essentially a no-op
+print("Simple #2 - about to import pyraf ...") # should be essentially a no-op
 import pyraf
-print("simple #3 - ver is ...")
+print("Simple #3 - ver is ...")
 print(pyraf.__version__)
-print("simple #4 - pwd is ...")
+print("Simple #4 - pwd is ...")
 iraf.pwd() # will print
-print("simple #5 - has IRAF is ...")
+print("Simple #5 - has IRAF is ...")
 from pyraf import irafinst
 print(str(irafinst.EXISTS))
 if not irafinst.EXISTS:
    sys.exit(0)
 
-print("simple #6 - imaccess to dev$pix is ... (requires IRAF)")
+print("Simple #6 - files output is ... (the rest require IRAF)")
+iraf.files('file_a.txt,file-b.txt,file.c.txt,,filed.txt')
+print("Simple #7 - loading imutil")
 iraf.images(_doprint=0) # load images
 iraf.imutil(_doprint=0) # load imutil
+print("Simple #8 - imaccess to dev$pix is ...")
 print(iraf.imaccess("dev$pix"))
-print("simple #7 - imheader of dev$pix is ... (requires IRAF)")
+print("Simple #9 - imheader of dev$pix is ...")
 iraf.imheader("dev$pix")
 
 sys.exit(0)
