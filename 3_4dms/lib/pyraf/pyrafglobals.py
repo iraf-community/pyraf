c3cc6908ee7857ab5577d69ac5bce8043dcc8682
stsci_iraf <stsci_iraf@stsci.edu>
2011-11-03 19:11:11 +0000

    tagging opus 2011_3 delivery 4
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/tags/opus_2011@1563 05fa7843-0683-0410-9e38-dfee850dc5be

---
+++ 3_4dms/lib/pyraf/pyrafglobals.py
@@ -0,0 +1,38 @@
+"""module pyrafglobals.py -- widely used PyRAF constants and objects
+
+pyrafDir        Directory with these Pyraf programs
+_use_ecl        Flag to turn on ECL mode in PyRAF
+
+This is defined so it is safe to say 'from pyrafglobals import *'
+
+$Id$
+
+Broken out from irafglobals.py which was signed "R. White, 2000 January 5"
+"""
+from __future__ import division # confidence high
+
+import os, sys
+_os = os
+_sys = sys
+del os, sys
+
+_use_ecl = _os.environ.get("PYRAF_USE_ECL", False)
+
+# -----------------------------------------------------
+# pyrafDir is directory containing this script
+# -----------------------------------------------------
+if __name__ == "__main__":
+    thisfile = _sys.argv[0]
+else:
+    thisfile = __file__
+# follow links to get to the real filename
+while _os.path.islink(thisfile):
+    thisfile = _os.readlink(thisfile)
+pyrafDir = _os.path.dirname(thisfile)
+del thisfile
+
+from stsci.tools.irafglobals import userWorkingHome
+if not pyrafDir: pyrafDir = userWorkingHome
+# change relative directory paths to absolute and normalize path
+pyrafDir = _os.path.normpath(_os.path.join(userWorkingHome, pyrafDir))
+del userWorkingHome
