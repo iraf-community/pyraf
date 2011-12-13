e87c0a2a31d4426e767f60feda82a897c4d71176
stsci_embray <stsci_embray@stsci.edu>
2011-12-13 15:25:06 +0000

    Adding dependency_links
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/branches/setup_refactoring@1581 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ except ImportError:
 
 setup(
     setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.2'],
+    dependency_links=['http://stsdas.stsci.edu/download/packages'],
     d2to1=True,
     use_2to3=True
 )
