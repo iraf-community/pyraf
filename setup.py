16534e0d2a5027ce88b0bc3982539653500e6af4
stsci_embray <stsci_embray@stsci.edu>
2012-04-16 16:12:16 +0000

    Updating setup requirement versions, and removing the easier_install command which is not used anymore.
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/branches/setup_refactoring@1686 05fa7843-0683-0410-9e38-dfee850dc5be

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.2'],
+    setup_requires=['d2to1>=0.2.6', 'stsci.distutils>=0.3'],
     dependency_links=['http://stsdas.stsci.edu/download/packages'],
     d2to1=True,
     use_2to3=True
