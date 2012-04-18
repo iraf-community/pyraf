c89d554ee78363e9c3070ce648a5a41a56dd15f3
stsci_iraf <stsci_iraf@stsci.edu>
2012-04-18 21:29:14 +0000

    tagging opus 2012_1 delivery 17
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/tags/opus_2012@1698 05fa7843-0683-0410-9e38-dfee850dc5be

---
+++ 1_17dms/lib/pyraf/iraf.py
@@ -0,0 +1,19 @@
+"""module iraf.py -- home for all the IRAF tasks and basic access functions
+
+$Id$
+
+R. White, 1999 Jan 25
+"""
+from __future__ import division # confidence high
+
+from iraffunctions import *
+
+# a few CL tasks have modified names (because they start with '_')
+import iraffunctions
+
+_curpack = iraffunctions.curpack
+_allocate = iraffunctions.clAllocate
+_deallocate = iraffunctions.clDeallocate
+_devstatus = iraffunctions.clDevstatus
+
+del iraffunctions
