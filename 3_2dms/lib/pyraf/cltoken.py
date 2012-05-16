4313a5cd1e16bbf9760a63eaea97ab8f1444f3e7
stsci_iraf <stsci_iraf@stsci.edu>
2012-05-16 16:05:56 +0000

    tagging opus 2012_3 delivery 2
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/tags/opus_2012@1728 05fa7843-0683-0410-9e38-dfee850dc5be

---
+++ 3_2dms/lib/pyraf/cltoken.py
@@ -0,0 +1,208 @@
+"""cltoken.py: Token definition for CL parser
+
+$Id$
+"""
+from __future__ import division # confidence high
+
+#  Copyright (c) 1998-1999 John Aycock
+#
+#  Permission is hereby granted, free of charge, to any person obtaining
+#  a copy of this software and associated documentation files (the
+#  "Software"), to deal in the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#
+#  The above copyright notice and this permission notice shall be
+#  included in all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#
+#       Token class for IRAF CL parsing
+#
+
+import string
+from stsci.tools.irafglobals import INDEF
+from stsci.tools import compmixin
+
+verbose = 0
+
+class Token(compmixin.ComparableMixin):
+    def __init__(self, type=None, attr=None, lineno=None):
+        self.type = type
+        self.attr = attr
+        self.lineno = lineno
+
+    #
+    #  Not all these may be needed:
+    #
+    #  _compare     required for GenericParser, required for
+    #                       GenericASTMatcher only if your ASTs are
+    #                       heterogeneous (i.e., AST nodes and tokens)
+    #  __repr__     recommended for nice error messages in GenericParser
+    #  __getitem__  only if you have heterogeneous ASTs
+    #
+    def _compare(self, other, method):
+        if isinstance(other, Token):
+            return method(self.type, other.type)
+        else:
+            return method(self.type, other)
+
+    def __hash__(self):
+        return hash(self.type)
+
+    def verboseRepr(self):
+        if self.attr:
+            return self.type + '(' + self.attr + ')'
+        else:
+            return self.type
+
+    def __repr__(self):
+        global verbose
+        if verbose:
+            return self.verboseRepr()
+        else:
+            if self.type in ["STRING", "QSTRING"]:
+                # add quotes to strings
+                # but replace double escapes with single escapes
+                return repr(self.attr).replace('\\\\','\\')
+            else:
+                rv = self.attr
+                if rv is None: rv = self.type
+                return rv
+
+    def __getitem__(self, i):
+        raise IndexError
+
+    def __len__(self):
+        return 0
+
+    def get(self):
+        """Return native representation of this token"""
+        if self.type == "INTEGER":
+            return self.__int__()
+        elif self.type == "FLOAT":
+            return self.__float__()
+        elif self.type in ["STRING","QSTRING"]:
+            return self.attr
+        elif self.type == "BOOL":
+            return self.bool()
+
+    # special conversions
+
+    def __str__(self):
+        rv = self.attr
+        if rv is None: rv = self.type
+        return rv
+
+    def __int__(self):
+        if self.type == "INTEGER":
+            return _str2int(self.attr)
+        elif self.type == "INDEF":
+            return int(INDEF)
+        elif self.type == "FLOAT":
+            # allow floats as values if they are exact integers
+            f = self.__float__()
+            i = int(f)
+            if float(i) == f: return i
+        elif self.type in ["STRING", "QSTRING"]:
+            try:
+                if self.attr == "":
+                    return int(INDEF)
+                elif self.attr[:1] == ')':
+                    # indirection to another parameter
+                    return self.attr
+                else:
+                    return _str2int(self.attr)
+            except Exception, e:
+                print 'Exception', str(e)
+                pass
+        raise ValueError("Cannot convert " +
+                self.verboseRepr() + " to int")
+
+    def __float__(self):
+        if self.type == "FLOAT":
+            # convert d exponents to e for Python
+            value = self.attr
+            i = value.find('d')
+            if i>=0:
+                value = value[:i] + 'e' + value[i+1:]
+            else:
+                i = value.find('D')
+                if i>=0:
+                    value = value[:i] + 'E' + value[i+1:]
+            return float(value)
+        elif self.type == "INTEGER":
+            # convert to int first because of octal, hex formats
+            return float(_str2int(self.attr))
+        elif self.type == "SEXAGESIMAL":
+            # convert d:m:s values directly to float
+            flist = self.attr.split(':')
+            flist.reverse()
+            value = float(flist[0])
+            for v in flist[1:]:
+                value = float(v) + value/60.0
+            return value
+        elif self.type == "INDEF":
+            return float(INDEF)
+        elif self.type in ["STRING", "QSTRING"]:
+            try:
+                if self.attr == "":
+                    return float(INDEF)
+                elif self.attr[:1] == ')':
+                    # indirection to another parameter
+                    return self.attr
+                else:
+                    return float(self.attr)
+            except (ValueError, TypeError):
+                pass
+        raise ValueError("Cannot convert " +
+                self.verboseRepr() + " to float")
+
+    def bool(self):
+        #XXX convert INTEGER to bool too?
+        if self.type == "BOOL":
+            return self.attr
+        elif self.type == "INDEF":
+            return INDEF
+        elif self.type in ["STRING", "QSTRING"]:
+            keyword = self.attr.lower()
+            if keyword in ["yes", "y"]:
+                return "yes"
+            elif keyword in ["no", "n"]:
+                return "no"
+            elif self.attr[:1] == ')':
+                # indirection to another parameter
+                return self.attr
+            elif keyword == "":
+                return INDEF
+        raise ValueError("Cannot convert " +
+                self.verboseRepr() + " to bool")
+
+def _str2int(value):
+    # convert integer string to python int
+    # handles IRAF octal, hex values
+    last = value[-1].lower()
+    if last == 'b':
+        # octal
+        return eval('0'+value[:-1])
+    elif last == 'x':
+        # hexadecimal
+        return eval('0x'+value[:-1])
+    # remove leading zeros on decimal values
+    i=0
+    for digit in value:
+        if digit != '0': break
+        i = i+1
+    else:
+        # all zeros
+        return 0
+    return int(value[i:])
---
+++ 3_2dms/src/xutil.c
@@ -0,0 +1,450 @@
+#include <Python.h>
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <stdio.h>
+#include <setjmp.h>
+#include <string.h>
+
+/* $Id$ */
+
+/* Windows and cursor manipulations not provided by Tkinter or any other
+** standard python library.  This file handles Python 3 as well.
+** see also: http://docs.python.org/py3k/extending
+** see also: http://docs.python.org/howto/cporting.html
+** see also: http://python3porting.com/cextensions.html
+*/
+
+/* the following macro is used to trap x window exceptions and trigger
+** a python exception in turn
+*/
+
+jmp_buf ErrorEnv;
+int xstatus;
+int (* oldErrorHandler)(Display *, XErrorEvent *);
+int (* oldIOErrorHandler)(Display *);
+char XErrorMsg[80];
+char ErrorPrefix[] = "XWindows Error!\n";
+char ErrorMsg[120];
+char IOError[] = "XWindows IO exception.";
+
+static Display *d;
+static int screen_num;
+static GC gc;
+static XWindowAttributes wa;
+static int last_win = -1;
+static Colormap cmap;
+static XColor colorfg, colorbg, color;
+
+#define TrapXlibErrors \
+    oldIOErrorHandler = XSetIOErrorHandler(&MyXlibIOErrorHandler); \
+    oldErrorHandler = XSetErrorHandler(&MyXlibErrorHandler); \
+    xstatus = setjmp(ErrorEnv); \
+    if ( xstatus != 0) { \
+      XSetIOErrorHandler(oldIOErrorHandler); \
+      XSetErrorHandler(oldErrorHandler); \
+      strncat(ErrorMsg,ErrorPrefix, 20); \
+      strncat(ErrorMsg,XErrorMsg, 80); \
+      PyErr_SetString(PyExc_EnvironmentError, ErrorMsg); \
+      return NULL; \
+    }
+
+#define RestoreOldXlibErrorHandlers \
+        XSetIOErrorHandler(oldIOErrorHandler); \
+        XSetErrorHandler(oldErrorHandler);
+
+int MyXlibErrorHandler(Display *d, XErrorEvent *myerror) {
+        XGetErrorText(d, myerror->error_code, XErrorMsg, 80);
+        longjmp(ErrorEnv, 1);
+        /* Pointless, but it shuts up some compiler warning messages */
+        return 0;
+}
+
+int MyXlibIOErrorHandler(Display *d) {
+  /* just put a constant string in the error message */
+  strncat(XErrorMsg,IOError,80);
+  longjmp(ErrorEnv, 1);
+  /* Pointless, but it shuts up some compiler warning messages */
+  return 0;
+}
+
+// rx,ry are unused by the X version of this; x,y are w.r.t. the given window
+void moveCursorTo(int win, int rx, int ry, int x, int y) {
+  Window w;
+  /*  Display *XOpenDisplay(char *); */
+  int s;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  w = (Window) win;
+  s = XGrabPointer(d,w,True,ButtonPressMask | EnterWindowMask,GrabModeSync,
+        GrabModeSync,None,None,CurrentTime);
+  XWarpPointer(d,None,w,0,0,0,0,x,y);
+  XUngrabPointer(d,CurrentTime);
+  XFlush(d);
+}
+
+PyObject *wrap_moveCursorTo(PyObject *self, PyObject *args) {
+  int w, rx, ry, x, y;
+  if (!PyArg_ParseTuple(args, "iiiii", &w, &rx, &ry, &x, &y))
+    return NULL;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  moveCursorTo(w, rx, ry, x, y);
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+int getFocalWindowID(void) {
+   Window w;
+   int revert;
+   /*  Display *XOpenDisplay(char *); */
+   if (d == NULL) {
+     printf("could not open XWindow display\n");
+     return -1;
+        }
+   XGetInputFocus(d,&w,&revert);
+   XFlush(d);
+   return (int) w;
+}
+
+int getDeepestVisual(void) {
+  XVisualInfo *visualList;
+  int i, visualsMatched, maxDepth;
+  maxDepth = 1;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return -1;
+  }
+  visualList = XGetVisualInfo (d, VisualNoMask, NULL, &visualsMatched);
+  for (i=0;i<visualsMatched;i++) {
+    if (visualList[i].depth > maxDepth) {
+      maxDepth = visualList[i].depth;
+    }
+  }
+  XFree(visualList);
+  XFlush(d);
+  return maxDepth;
+}
+
+PyObject *wrap_getDeepestVisual(PyObject *self, PyObject *args) {
+  int depth;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  depth = getDeepestVisual();
+  RestoreOldXlibErrorHandlers /* macro */
+  return Py_BuildValue("i",depth);
+}
+
+PyObject *wrap_getFocalWindowID(PyObject *self, PyObject *args) {
+  int result;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  result = getFocalWindowID();
+  RestoreOldXlibErrorHandlers /* macro */
+  return Py_BuildValue("i",result);
+}
+
+void setFocusTo(int win) {
+  Window w;
+  /* Display *XOpenDisplay(char *); */
+  w = (Window) win;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  XSetInputFocus(d,w,0,CurrentTime);
+  XFlush(d);
+}
+
+PyObject *wrap_setFocusTo(PyObject *self, PyObject *args) {
+  int win;
+  if (!PyArg_ParseTuple(args,"i", &win))
+    return NULL;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  setFocusTo(win);
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+void setBackingStore(int win) {
+  Window w;
+  XWindowAttributes wa;
+  XSetWindowAttributes  NewWinAttributes;
+  Status XGetWindowAttributes(Display *, Window, XWindowAttributes *);
+  int XChangeWindowAttributes(Display *, Window, unsigned long,
+                               XSetWindowAttributes *);
+  w = (Window) win;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  XGetWindowAttributes(d, w, &wa);
+  if (XDoesBackingStore(wa.screen) != NotUseful) {
+    NewWinAttributes.backing_store = Always;
+    XChangeWindowAttributes(d, w, CWBackingStore, &NewWinAttributes);
+  }
+  XFlush(d);
+}
+
+PyObject *wrap_setBackingStore(PyObject *self, PyObject *args) {
+  int win;
+  if (!PyArg_ParseTuple(args,"i", &win))
+    return NULL;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+    setBackingStore(win);
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+void getWindowAttributes(int win, XWindowAttributes *winAttr, char **visual) {
+  Window w;
+  XVisualInfo visual_info;
+  static char *visual_class[] = {
+          "StaticGray",
+          "GrayScale",
+          "StaticColor",
+          "PseudoColor",
+          "TrueColor",
+          "DirectColor"
+  };
+  int i;
+  int screen_num;
+  Status XGetWindowAttributes(Display *, Window, XWindowAttributes *);
+  i = 5;
+  w = (Window) win;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  XGetWindowAttributes(d, w, winAttr);
+  screen_num = DefaultScreen(d);
+  while (!XMatchVisualInfo(d, screen_num, DefaultDepth(d, screen_num),
+                 i--, &visual_info));
+  *visual = visual_class[++i];
+}
+
+PyObject *wrap_getWindowAttributes(PyObject *self, PyObject *args) {
+        int win, viewable;
+        XWindowAttributes wa;
+        char *visual = NULL;
+        if (!PyArg_ParseTuple(args,"i", &win))
+                return NULL;
+        TrapXlibErrors /* macro code to handle xlib exceptions */
+        getWindowAttributes(win, &wa, &visual);
+        if (wa.map_state == IsViewable) {
+          viewable = 1;
+        } else {
+          viewable = 0;
+        }
+        RestoreOldXlibErrorHandlers /* macro */
+        return Py_BuildValue("{s:i,s:i,s:i,s:i,s:i,s:i,s:i,s:i,s:s}",
+                "x", wa.x,
+                "y", wa.y,
+                "rootID", (int) wa.root,
+                "width", wa.width,
+                "height", wa.height,
+                "borderWidth", wa.border_width,
+                                                 "viewable", viewable,
+                "depth", wa.depth,
+                "visualClass", visual);
+}
+
+PyObject *wrap_getPointerPosition(PyObject *self, PyObject *args) {
+        int win;
+        Window w;
+        Bool inScreen;
+        Window root, child;
+        int root_x, root_y, win_x, win_y;
+        unsigned int mask;
+        Bool XQueryPointer(Display *, Window, Window *, Window *,
+                int *, int *, int *, int *, unsigned int *);
+        if (!PyArg_ParseTuple(args,"i", &win))
+                return NULL;
+        w = (Window) win;
+        TrapXlibErrors /* macro code to handle xlib exceptions */
+        if (d == NULL) {
+                printf("could not open XWindow display\n");
+                RestoreOldXlibErrorHandlers /* macro */
+                return NULL;
+        }
+        inScreen = XQueryPointer(d, w, &root, &child, &root_x, &root_y,
+                &win_x, &win_y, &mask);
+        RestoreOldXlibErrorHandlers /* macro */
+        return Py_BuildValue("{s:i,s:i,s:i,s:i,s:i,s:i,s:i}",
+                "inScreen", (int) inScreen,
+                "rootID", (int) root,
+                "childID", (int) child,
+                "root_x", root_x,
+                "root_y", root_y,
+                "win_x", win_x,
+                "win_y", win_y);
+}
+
+PyObject *wrap_getParentID(PyObject *self, PyObject *args) {
+
+        int win;
+        Window w;
+        Window root, parent, *children;
+        unsigned int nchildren;
+        /*      Display *XOpenDisplay(char *); */
+        Status XQueryTree(Display *, Window, Window *, Window *,
+                Window **, unsigned int *);
+        if (!PyArg_ParseTuple(args,"i", &win))
+                return NULL;
+        w = (Window) win;
+        if ((w==PointerRoot) | (w==None)) {
+          return Py_BuildValue("i", (int) w);
+        }
+        TrapXlibErrors /* macro code to handle xlib exceptions */
+        if (d == NULL) {
+                printf("could not open XWindow display\n");
+                RestoreOldXlibErrorHandlers /* macro */
+                return NULL;
+        }
+        XQueryTree(d, w, &root, &parent, &children, &nchildren);
+        XFree(children);
+        RestoreOldXlibErrorHandlers /* macro */
+        if (root == parent) {
+          Py_INCREF(Py_None);
+          return Py_None;
+        } else {
+          return Py_BuildValue("i", (int) parent);
+        }
+}
+
+void initXGraphics(void) {
+
+  d = XOpenDisplay(NULL);
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  screen_num = DefaultScreen(d);
+}
+
+PyObject *wrap_initXGraphics(PyObject *self, PyObject *args) {
+
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  initXGraphics();
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+void closeXGraphics(void) {
+
+  XCloseDisplay(d);
+}
+
+PyObject *wrap_closeXGraphics(PyObject *self, PyObject *args) {
+
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  closeXGraphics();
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+void drawCursor(int win, double x, double y, int width, int height) {
+  /* plot cursor at x and y where x,y are normalized (range from 0 to 1) */
+  Window w;
+
+  w = (Drawable) win;
+  if (d == NULL) {
+    printf("could not open XWindow display\n");
+    return;
+  }
+  /* only get a graphics context if the window id changes */
+  if (win != last_win) {
+    last_win = win;
+    gc = XCreateGC(d, w, 0, NULL);
+    if (!XGetWindowAttributes(d, w, &wa)) {
+      printf("Problem getting window attributes\n");
+      return;
+    }
+    cmap = wa.colormap;
+    if (!XParseColor(d, cmap, "red", &colorfg)) {
+      printf("could not parse color string\n");
+      return;
+    }
+    if (!XParseColor(d, cmap, "black", &colorbg)) {
+      printf("could not parse color string\n");
+      return;
+    }
+    if (!(XAllocColor(d, cmap, &colorfg) && XAllocColor(d, cmap, &colorbg))) {
+       printf("Problem allocating colors for cursor color determination\n");
+       return;
+    }
+    color.pixel = colorfg.pixel ^ colorbg.pixel;
+    XSetFunction(d, gc, GXxor);
+    XSetForeground(d, gc, color.pixel);
+  }
+  /*if (!XGetGeometry(d,w,&wroot, &xr, &yr, &width, &height, &border, &depth)) {
+    printf("could not get window geometry\n");
+    return;
+  }*/
+  XDrawLine(d, w, gc, (int) (x*width), 0, (int) (x*width),  height);
+  XDrawLine(d, w, gc, 0, (int) ((1.-y)*height),  width, (int) ((1.-y)*height));
+  XFlush(d);
+}
+
+PyObject *wrap_drawCursor(PyObject *self, PyObject *args) {
+
+  double x, y;
+  int width, height;
+  int win;
+
+  if (!PyArg_ParseTuple(args, "iddii", &win, &x, &y, &width, &height))
+    return NULL;
+  TrapXlibErrors /* macro code to handle xlib exceptions */
+  drawCursor(win, x, y, width, height);
+  RestoreOldXlibErrorHandlers /* macro */
+  Py_INCREF(Py_None);
+  return Py_None;
+}
+
+static PyMethodDef xutil_funcs[] = {
+  { "moveCursorTo",wrap_moveCursorTo, 1},
+  { "getFocalWindowID",wrap_getFocalWindowID, 1},
+  { "setFocusTo",wrap_setFocusTo, 1},
+  { "setBackingStore",wrap_setBackingStore, 1},
+  { "getWindowAttributes",wrap_getWindowAttributes, 1},
+  { "getPointerPosition",wrap_getPointerPosition, 1},
+  { "getParentID",wrap_getParentID, 1},
+  { "getDeepestVisual", wrap_getDeepestVisual, 1},
+  { "initXGraphics",wrap_initXGraphics, 1},
+  { "closeXGraphics",wrap_closeXGraphics, 1},
+  { "drawCursor",wrap_drawCursor, 1},
+  {NULL, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "xutil",
+        NULL,
+        -1,
+        xutil_funcs,
+        NULL, NULL, NULL, NULL,
+};
+PyObject* PyInit_xutil(void)
+#else
+void initxutil(void)
+#endif
+{
+   PyObject *m;
+#if PY_MAJOR_VERSION >= 3
+   m = PyModule_Create(&moduledef);
+#else
+   m = Py_InitModule("xutil", xutil_funcs);
+#endif
+
+/* in Py2.*: just return */
+#if PY_MAJOR_VERSION >= 3
+   return m;
+#endif
+}
