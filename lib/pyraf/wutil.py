87810642259a71a6d779b7f06bda48af801dfaba
sontag <sontag@stsci.edu>
2013-12-13 15:54:47 +0000

    add helpful debug msg in case where xutil didnt build
    git-svn-id: https://aeon.stsci.edu/ssb/svn/pyraf/trunk@2105 05fa7843-0683-0410-9e38-dfee850dc5be

--- lib/pyraf/wutil.py
+++ lib/pyraf/wutil.py
@@ -541,6 +541,9 @@ def dumpspecs(outstream = None, skip_volatiles = False):
         out += "\nimported aqutil = "+str(bool(_has_aqutil))
         out += "\nimported xutil = "+str(bool(_has_xutil))
 
+        if capable.OF_GRAPHICS and WUTIL_USING_X and not bool(_has_xutil):
+            # quick debug help here for case where xutil didn't build
+            out += '\n\tWARNING!  PyRAF is missing the "xutil" library.  See PyRAF FAQ 1.9'
         if 'PYRAFGRAPHICS' in os.environ:
             val = os.environ['PYRAFGRAPHICS']
             out += "\nPYRAFGRAPHICS = "+val
