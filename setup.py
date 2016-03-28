7deb9abb410d5d441b7877fe8b0e69b21d1c8de8
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-28 19:47:14 -0400

    Update RECON

--- setup.py
+++ setup.py
@@ -1,15 +1,56 @@
 #!/usr/bin/env python
+import recon.release
+from glob import glob
+from numpy import get_include as np_include
+from setuptools import setup, find_packages, Extension
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+
+version = recon.release.get_info()
+recon.release.write_template(version, 'lib/pyraf')
 
 setup(
-    setup_requires=['d2to1>=0.2.3', 'stsci.distutils>=0.3.2'],
-    d2to1=True,
-    use_2to3=True,
-    zip_safe=False
+    name = 'pyraf',
+    version = version.pep386,
+    author = 'Rick White, Perry Greenfield, Chris Sontag',
+    author_email = 'help@stsci.edu',
+    description = 'Provides a Pythonic interface to IRAF that can be used in place of the existing IRAF CL',
+    url = 'https://github.com/spacetelescope/pyraf',
+    classifiers = [
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = [
+        'astropy',
+        'nose',
+        'numpy',
+        'scipy',
+        'sphinx',
+    ],
+
+    package_dir = {
+        '':'lib'
+    },
+    packages = find_packages('lib'),
+    package_data = {
+        'pyraf': [
+            'data/*',
+        ]
+    },
+    scripts=[
+        'scripts/*'
+    ],
+    ext_modules=[
+        Extension('pyraf.sscanfmodule',
+            ['src/sscanfmodule.c'],
+            optional=True,
+            fail_message='If this is Windows, it is ok.'),
+
+        Extension('pyraf.xutilmodule',
+            ['src/xutil.c'],
+            libraries=['X11']),
+    ],
 )
