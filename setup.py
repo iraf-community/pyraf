11e07586aef58578afa7b5f4c8d720527c339ea6
Joseph Hunkeler <jhunkeler@gmail.com>
2016-03-31 16:37:17 -0400

    Implement RELIC

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import recon.release
+import relic.release
 from glob import glob
 from numpy import get_include as np_include
 from setuptools import setup, find_packages, Extension
 
 
-version = recon.release.get_info()
-recon.release.write_template(version, 'lib/pyraf')
+version = relic.release.get_info()
+relic.release.write_template(version, 'lib/pyraf')
 
 setup(
     name = 'pyraf',
