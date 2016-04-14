3f7265f27c2836471cfce9ef88dbe6e863e75a7d
Joseph Hunkeler <jhunkeler@gmail.com>
2016-04-14 14:55:16 -0400

    Implement RELIC bootstrap

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
-import relic.release
+import os
+import subprocess
+import sys
 from glob import glob
 from numpy import get_include as np_include
 from setuptools import setup, find_packages, Extension
+
+
+if os.path.exists('relic'):
+    sys.path.insert(1, 'relic')
+    import relic.release
+else:
+    try:
+        import relic.release
+    except ImportError:
+        try:
+            subprocess.check_call(['git', 'clone',
+                'https://github.com/jhunkeler/relic.git'])
+            sys.path.insert(1, 'relic')
+            import relic.release
+        except subprocess.CalledProcessError as e:
+            print(e)
+            exit(1)
 
 
 version = relic.release.get_info()
@@ -41,7 +60,7 @@ setup(
         ]
     },
     scripts=[
-        'scripts/*'
+        '':'scripts/*'
     ],
     ext_modules=[
         Extension('pyraf.sscanfmodule',
