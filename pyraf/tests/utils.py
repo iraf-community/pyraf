4e7d4ee389002a212f7ef2bc71a560a5701a7a60
Ole Streicher <olebole@debian.org>
2021-09-07 12:23:44 +0200

    Parametrize test_cli tests

--- pyraf/tests/utils.py
+++ pyraf/tests/utils.py
@@ -20,22 +20,3 @@ else:
         HAS_STSDAS = False
     else:
         HAS_STSDAS = True
-
-
-def diff_outputs(fin, reffile):
-    """Compare output lines with reference file."""
-    if isinstance(fin, list):
-        lines = fin
-    else:
-        with open(fin) as f:
-            lines = f.readlines()
-
-    ans = get_pkg_data_contents(reffile).split(os.linesep)
-    all_bad_lines = []
-
-    for x, y in zip(lines, ans):
-        if x.strip(os.linesep) != y:
-            all_bad_lines.append(f'{x} : {y}')
-
-    if len(all_bad_lines) > 0:
-        raise AssertionError(os.linesep.join(all_bad_lines))
